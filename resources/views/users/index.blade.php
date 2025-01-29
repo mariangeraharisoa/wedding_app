e05d97b0af3ba4d838b8a39b98ef4b7ac4c94f20
RaharisoaLalainaMariange <165947722+RaharisoaLalainaMariange@users.noreply.github.com>
2025-01-30 00:08:28 +0300

    css

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href=".css">
-</head>
-<body>
-    
-</body>
-</html>
 @extends('layouts.app')
 
 @section('title', 'Liste des utilisateurs')
